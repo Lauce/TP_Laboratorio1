9a30b8845dcec3e4722be40999e85f0f31f2ffbf
Lauce <vivanlaura@gmail.com>
2016-09-22 20:23:21 -0300

    Add files via upload

--- funciones.h
+++ funciones.h
@@ -1,15 +1,21 @@
 
 
-#ifndef FUNCIONES_H_INCLUDED
-#define FUNCIONES_H_INCLUDED
+typedef struct
+{
+    char nombre[50];
+    int edad;
+    int dni;
+    int estado;
+
+} ePersona;
 
 
-
-#endif // FUNCIONES_H_INCLUDED
-
-float sumarNumeros(float, float);
-float restarNumeros(float, float);
-float dividirNumeros(float, float);
-float multiplicarNumeros(float, float);
-int factorial(int);
-
+void inicializarVec(ePersona vector[], int length);
+int revisarEstado(ePersona vector[],int length, int *indice);
+void cargarAlta(ePersona* vector, int length, int indiceLibre);
+int revisarDni(ePersona vector[],int length, int parametro, int *indice);
+void borrarDatos(ePersona[],int);
+void ordenarNombre(ePersona[],int);
+int esUnNumero(char str[]);
+int soloLetras (char string[]);
+void graficoEdad(ePersona vector[], int length);
